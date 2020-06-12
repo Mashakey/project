2a446e9a04984558da03972caa82cb7c10ba554a
Masha <marysya753@mail.ru>
2020-06-12 19:13:19 +0300

    saved imena

---
+++ Assets/asdf.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public static class TempClass
+{
+    public static string player1;
+    public static string player2;
+
+}
+public class asdf : MonoBehaviour
+{
+    public InputField f1;
+    public InputField f2;
+    public static string player1;
+    public static string player2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void players()
+    {
+        TempClass.player1 = f1.text;
+        TempClass.player2 = f2.text;
+    }
+}
---
+++ Assets/p1_event.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class p1_event : MonoBehaviour
+{
+    public Text img1;
+    public Text img2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        q();
+        
+    }
+    public void q()
+    {
+        img1.text = TempClass.player1;
+        img2.text = TempClass.player2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
