c9caa844bd30177f7c3562246347660126ef9b42
Masha <marysya753@mail.ru>
2020-06-13 18:46:03 +0300

    lastv1

--- Assets/asdf.cs
+++ Assets/asdf.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+
 public static class TempClass
 {
-    public static string player1;
+    public static string player1 = "asd";
     public static string player2;
-
+    public static int settimer;
+    public static int win;
 }
+
 public class asdf : MonoBehaviour
 {
     public InputField f1;
     public InputField f2;
+    public InputField f3;
+
     public static string player1;
     public static string player2;
     // Start is called before the first frame update
@@ -30,5 +36,6 @@ public class asdf : MonoBehaviour
     {
         TempClass.player1 = f1.text;
         TempClass.player2 = f2.text;
+        TempClass.settimer = Convert.ToInt32(f3.text);
     }
 }
---
+++ Assets/sdatsya.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class sdatsya : MonoBehaviour
+{
+	
+	public void ChangeScene(string sceneName)
+	{
+		if (BoardMeneger.isWiteTurn != true)
+        {
+			TempClass.win = 1;
+        } else
+        {
+			TempClass.win = 0;
+        }
+		SceneManager.LoadScene(7);
+	}
+	public void Exit()
+	{
+		Application.Quit();
+	}
+	
+}
